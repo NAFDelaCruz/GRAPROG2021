9d25471adf73dfb216a320f67efd4f0df3052698
Niel Dela Cruz <nielangelodelacruz@gmail.com>
2021-10-13 14:49:32 +0800

    New Mechanic patch (Unfinished)

--- GRAPROG Game/Assets/Scripts/PlayerController.cs
+++ GRAPROG Game/Assets/Scripts/PlayerController.cs
@@ -1,30 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class PlayerController : MonoBehaviour
 {
     public SpriteRenderer PlayerSprite;
+    public SpriteRenderer CapeSprite;
     public Animator PlayerAnimator;
+    public Animator CapeAnimator;
+    public GameObject Lantern;
     public float PlayerSpeed;
 
-    // Update is called once per frame
+    [HideInInspector]
+    public float LightRadius;
+
+    void Start()
+    {
+        LightRadius = Lantern.GetComponent<Light2D>().pointLightOuterRadius;
+    }
+    
     void Update()
     {
+        float LanternPower = Input.GetAxis("Mouse ScrollWheel") * 5f;
+
+        if (LanternPower != 0)
+        {
+            LightRadius = Mathf.Clamp(LightRadius += LanternPower, 1.0f, 4.5f);
+            Lantern.GetComponent<Light2D>().pointLightOuterRadius = LightRadius;
+        }
+
         float MovePlayer = Input.GetAxis("Horizontal");
 
-        if (Input.GetAxis("Horizontal") > 0)
+        if (MovePlayer > 0)
         {
             transform.Translate(Vector2.right * Time.deltaTime * PlayerSpeed * MovePlayer);
             PlayerSprite.flipX = false;
+            CapeSprite.flipX = false;
             PlayerAnimator.SetFloat("Moving", Mathf.Abs(MovePlayer));
+            CapeAnimator.SetFloat("Moving", Mathf.Abs(MovePlayer));
         }
 
-        if (Input.GetAxis("Horizontal") < 0)
+        if (MovePlayer < 0)
         {
             transform.Translate(Vector2.left * Time.deltaTime * PlayerSpeed * -MovePlayer);
             PlayerSprite.flipX = true;
+            CapeSprite.flipX = true;
             PlayerAnimator.SetFloat("Moving", Mathf.Abs(MovePlayer));
+            CapeAnimator.SetFloat("Moving", Mathf.Abs(MovePlayer));
         }
     }
 }
--- GRAPROG Game/Assets/Scripts/PlayerState.cs
+++ GRAPROG Game/Assets/Scripts/PlayerState.cs
@@ -5,22 +5,31 @@ using UnityEngine.UI;
 
 public class PlayerState : MonoBehaviour
 {
+    [Header("Set Components")]
     public Slider FuelBar;
     public Slider FrostBar;
     public Material FrostEffect;
     public Image FrostWarning;
-    public float Health;
-    public float OvertimeFrostDamage;
-    public float FrostDamageDelay;
+    public PlayerController PlayerControllerScript;
 
-    private float _damage = 10;
+    [Header("Game Values")]
+    public float Health;
+    [Tooltip("The amount of Frost taken overtime")]
+    public float OvertimeFrostDamage;
+    [Tooltip("The number of seconds before Frost increases")]
+    public float FrostEffectsDelay;
+    [Tooltip("This value is divided by the Lantern's Point Light Outer Radius which results into the delay of HP deplete.")]
+    public float HPDepleteRate;
+    
     private float _maxHealth;
     private float _maxFrostDamage;
     private float _currentFrostDamage;
     private float _frostDamageExecuteTime = 0;
+    private float _hpDepleteTime = 0;
     private float _defrostExecuteTime = 0;
     private float _transparency;
     private float _iceState;
+    private bool _isTakingFrost = false;
 
     void Start()
     {
@@ -32,64 +41,75 @@ public class PlayerState : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && Health > 0)
-        {
-            Health -= _damage;
-            FuelBar.value = _maxHealth - Health;
-        }
-
-        if (Input.GetKeyDown(KeyCode.H) && Health < _maxHealth)
-        {
-            Health += 10;
-            FuelBar.value = _maxHealth - Health;
-        }
-
-        if (Health == 0 && Time.time > _frostDamageExecuteTime && _currentFrostDamage > 0)
-        {
-            _currentFrostDamage -= OvertimeFrostDamage;
-            FrostBar.value = _currentFrostDamage;
-            _frostDamageExecuteTime = Time.time + FrostDamageDelay;
-        }
-        else if (Health > 0 && Time.time > _defrostExecuteTime && _currentFrostDamage < _maxFrostDamage)
-        {
-            if (Health == 100 && Health >= 75)
-            {
-                _currentFrostDamage += 5;
-                _defrostExecuteTime = Time.time + 0.5f;
-            }
-            else if (Health < 75 && Health >= 50 )
-            {
-                _currentFrostDamage += 3;
-                _defrostExecuteTime = Time.time + 1f;
-            }
-            else if (Health < 50 && Health >= 1)
-            {
-                _currentFrostDamage += 1;
-                _defrostExecuteTime = Time.time + 1.5f;
-            }
-
-            FrostBar.value = _currentFrostDamage;
-        }
-
-        if (_currentFrostDamage > 49 && _currentFrostDamage <= 99 && Health == 0)
+        if (_currentFrostDamage < 0 && _isTakingFrost)
         {
             FrostWarning.color = new Color(1, 1, 1, _transparency);
             float pingpong = Mathf.PingPong(Time.time, 0.5f);
             _transparency = Mathf.Lerp(0f, 0.65f, pingpong);
         }
-        else if (_currentFrostDamage >= 0 && _currentFrostDamage <= 49 && Health == 0 && _iceState < 1)
+        
+        if (Input.GetKeyDown(KeyCode.H) && Health < _maxHealth)
+        {
+            Health = Mathf.Clamp(Health += 10, 0, 100);
+            FuelBar.value = _maxHealth - Health;
+        }
+
+        if (Time.time >= _hpDepleteTime && Health > 0)
+        {
+            Health = Mathf.Clamp(Health -= 1, 0, 100);
+            FuelBar.value = _maxHealth - Health;
+            _hpDepleteTime = Time.time + (HPDepleteRate / PlayerControllerScript.LightRadius);
+        }
+
+        //Frost Effect
+        if ((PlayerControllerScript.LightRadius <= 3.0f && PlayerControllerScript.LightRadius > 2.0f) && (_currentFrostDamage <= 100 && _currentFrostDamage > 75) && Time.time > _frostDamageExecuteTime)
+        {
+            AddFrostEffecT();
+            _isTakingFrost = true;
+        }
+        else if ((PlayerControllerScript.LightRadius <= 2.0f && PlayerControllerScript.LightRadius >= 1f) && (_currentFrostDamage <= 100 && _currentFrostDamage > 50) && Time.time > _frostDamageExecuteTime)
+        {
+            AddFrostEffecT();
+            _isTakingFrost = true;
+        }
+
+        //Defrost Effect 
+        if (PlayerControllerScript.LightRadius > 3.0f && (_currentFrostDamage < 100 && _currentFrostDamage >= 50) && Time.time > _defrostExecuteTime)
+        {
+            DefrostEffecT();
+        }
+        else if ((PlayerControllerScript.LightRadius <= 3.0f && PlayerControllerScript.LightRadius > 2f) && (_currentFrostDamage < 75 && _currentFrostDamage >= 50) && Time.time > _defrostExecuteTime)
+        {
+            DefrostEffecT();
+        } 
+
+        if (Health == 0 && Time.time > _frostDamageExecuteTime && _currentFrostDamage > 0)
+        {
+            AddFrostEffecT();
+        }
+
+        if (_currentFrostDamage >= 0 && _currentFrostDamage <= 49)
         {
             FrostWarning.color = new Color(1, 1, 1, 0);
-            FrostEffect.SetFloat("_Fade", _iceState += 0.1f * Time.deltaTime);
+            FrostEffect.SetFloat("_Fade", _iceState = Mathf.Clamp(_iceState += 0.1f, 0, 1) * Time.deltaTime);
         }
         else if (Health > 0 && _iceState > 0)
         {
-            FrostEffect.SetFloat("_Fade", _iceState -= 0.1f * Time.deltaTime);
+            FrostEffect.SetFloat("_Fade", _iceState = Mathf.Clamp(_iceState -= 0.1f, 0, 1) * Time.deltaTime);
         }
+    }
 
-        if (Health > 100) Health = 100;
-        if (_currentFrostDamage > 100) _currentFrostDamage = 100;
-        if (_iceState > 1) _iceState = 1;
-        if (_iceState < 0) _iceState = 0;
+    void AddFrostEffecT()
+    {
+        _currentFrostDamage -= OvertimeFrostDamage;
+        FrostBar.value = _currentFrostDamage;
+        _frostDamageExecuteTime = Time.time + FrostEffectsDelay;
+    }
+
+    void DefrostEffecT()
+    {
+        _currentFrostDamage += OvertimeFrostDamage;
+        FrostBar.value = _currentFrostDamage;
+        _defrostExecuteTime = Time.time + FrostEffectsDelay;
     }
 }
