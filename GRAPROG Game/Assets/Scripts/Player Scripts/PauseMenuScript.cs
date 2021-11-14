739313a56750e2592b34fc3c9e1e647b0cdb0b8a
Niel Dela Cruz <nielangelodelacruz@gmail.com>
2021-11-14 15:35:09 +0800

    UI Additional Complete

--- GRAPROG Game/Assets/Scripts/Player Scripts/PauseMenuScript.cs
+++ GRAPROG Game/Assets/Scripts/Player Scripts/PauseMenuScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PauseMenuScript : MonoBehaviour
 {
     public List<Sprite> ImageSequences;
+    public List<GameObject> ImageSequencesTexts;
     public Image Page;
     public GameObject HelpPage;
     public GameObject PauseMenu;
@@ -13,6 +14,7 @@ public class PauseMenuScript : MonoBehaviour
     public PlayerController PlayerControllerScript;
 
     private int _index = 0;
+    private int _textIndex = 0;
     private bool _isPaused = false;
 
     public void Update()
@@ -25,18 +27,32 @@ public class PauseMenuScript : MonoBehaviour
         {
             Unpause();
         }
+
+        for (int _textIndex = 0; _textIndex < ImageSequencesTexts.Capacity; _textIndex++)
+        {
+            if (_textIndex == _index)
+            {
+                ImageSequencesTexts[_textIndex].SetActive(true);
+            }
+            else if (_textIndex != _index)
+            {
+                ImageSequencesTexts[_textIndex].SetActive(false);
+            }
+        }
     }
 
     public void NextPage()
     {
         _index = Mathf.Clamp(_index + 1, 0, ImageSequences.Capacity - 1);
         Page.sprite = ImageSequences[_index];
+        _textIndex++;
     }
 
     public void LastPage()
     {
         _index = Mathf.Clamp(_index - 1, 0, ImageSequences.Capacity - 1);
         Page.sprite = ImageSequences[_index];
+        _textIndex--;
     }
 
     public void OpenHelpPage()
